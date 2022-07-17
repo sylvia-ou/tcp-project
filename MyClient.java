02773def65669edbe6771c1f8f4b16ba59035841
Sylvia <sylviaou6@gmail.com>
2022-07-16 20:50:49 -0700

    Update MyClient.java

--- MyClient.java
+++ MyClient.java
@@ -2,26 +2,83 @@ import java.net.*;
 import java.io.*;
 
 public class MyClient {
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
 
-    public void startConnection(String ip, int port) {
-        clientSocket = new Socket(ip, port);
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    //init socket and IO
+    private Socket socket = null;
+    private DataInputStream in = null;
+    private DataOutputStream out = null;
+
+    public MyClient(String ip, int port)
+    {
+        try
+        {
+            //establish connection
+            socket = new Socket(ip, port);
+            System.out.println("network");
+
+            //to server
+            out = new DataOutputStream(socket.getOutputStream());
+
+            //from server
+            in = new DataInputStream(socket.getInputStream());
+        }
+        catch(UnknownHostException e)
+        {
+            System.out.println(e);
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+
+        try
+        {
+            //write to server
+            out.writeUTF("test");
+            System.out.println("Packet sent");
+            out.flush();
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+
+        /*
+        for(int i = 1; i <= 5; i++)
+        {
+            //line to send to server (each line is an int value casted to a string)
+            String line = String.valueOf(i);
+            try
+            {
+                //write to server
+                out.writeUTF(line);
+                System.out.println("Packet " + i + " sent");
+                out.flush();
+            }
+            catch(IOException e)
+            {
+                System.out.println(e);
+            }
+        }
+        */
+
+        //close the connection
+        try
+        {
+            in.close();
+            out.close();
+            socket.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
     }
 
-    public String sendMessage(String msg) {
-        out.println(msg);
-        String resp = in.readLine();
-	    //test
-        return resp;
-    }
-
-    public void stopConnection() {
-        in.close();
-        out.close();
-        clientSocket.close();
+    public static void main(String args[])
+    {
+        //set address to your IP address
+        String address = "192.168.1.119";
+        MyClient client = new MyClient(address, 158);
     }
 }
